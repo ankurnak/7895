3b616a261301ece40012ba6ea8a8f5104a4f4601
ankurnak <112162607+ankurnak@users.noreply.github.com>
2023-08-24 21:33:10 +0300

    Add files via upload

---
+++ src/pages/Blog/PostCard/PostCard.js
@@ -0,0 +1,46 @@
+import './PostCard.css';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import EditIcon from '@material-ui/icons/Edit';
+
+export const PostCard = ({
+  title,
+  description,
+  liked,
+  likePost,
+  deletePost,
+  handleEditFormShow,
+  handleSelectPost,
+  isAdmin,
+}) => {
+  const showEditForm = () => {
+    handleSelectPost();
+    handleEditFormShow();
+  };
+
+  const heartFill = liked ? 'crimson' : 'black';
+
+  return (
+    <div className='post'>
+      <div className='postContent'>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <div>
+          <button onClick={likePost}>
+            <FavoriteIcon style={{ fill: heartFill }} />
+          </button>
+        </div>
+      </div>
+      {isAdmin && (
+        <div className='postControl'>
+          <button className='editBtn' onClick={showEditForm}>
+            <EditIcon />
+          </button>
+          <button className='deleteBtn' onClick={deletePost}>
+            <DeleteForeverIcon />
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
